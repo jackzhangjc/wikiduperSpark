9a01e4fe8fff343decb0dd300d4fc9f0d1cd6f18
miracle1949 <jackzhangjc@gmail.com>
2017-05-29 19:29:14 -0400

    multi hashing

--- src/main/Scala/spark/hash/application.scala
+++ src/main/Scala/spark/hash/application.scala
@@ -9,6 +9,10 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+
 class confApplication(args: Seq[String]) extends ScallopConf(args) {
   mainOptions = Seq(input, output)
   val input = opt[String](descr = "input path", required = true)
@@ -20,6 +24,8 @@ class confApplication(args: Seq[String]) extends ScallopConf(args) {
   val K = opt[Int](descr = "K", required = false, default = Some(10))
   val N = opt[Int](descr = "N", required = false, default = Some(10))
   val SHINGLELEN = opt[Int](descr = "SHINGLELEN", required = false, default = Some(12))
+  val rseed = opt[Int](descr = "rseed", required = false, default = Some(112345))
+  val NHASHOUTPUTBITS = opt[Int](descr = "NHASHOUTPUTBITS", required = false, default = Some(30))
   //val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
   verify()
 }
@@ -43,6 +49,19 @@ object Application {
     val SHINGLELEN = args.SHINGLELEN()
     val MAXLEN = args.MAXLEN()
     val MINLEN = args.MINLEN()
+    val rseed = args.rseed()
+    val NHASHOUTPUTBITS = args.NHASHOUTPUTBITS()
+    val N = args.N()
+    val K = args.K()
+    var seeds = ListBuffer[Long]()
+    var r = new Random(rseed)
+    for (i <- 0 to NHASH - 1) {
+      seeds += r.nextLong()
+    }
+    val sigseed = r.nextLong()
+    val hasher = Hasher.create(NHASHOUTPUTBITS, seeds.toList)
+
+
 
     val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
@@ -54,14 +73,14 @@ object Application {
         //line_list[0]
         (line_list(0), line_list(1))
       })
-      .map(t => {
+      .flatMap(t => {
         val id = t._1
         val sentence = t._2
 
         // Minhash vector
         // Initialize the minhash vector
-        //var MINHASH = List.fill(NHASH)(Long.MaxValue)
-        var MINHASH = Long.MaxValue
+        var MINHASH = ListBuffer.fill(NHASH)(Long.MaxValue)
+        //var MINHASH = Long.MaxValue
         // Create shingle.
 
         // Hash
@@ -72,42 +91,57 @@ object Application {
             val shingle = sentence.substring(i, i + SHINGLELEN)
 
             // Create the list of hash values
-            //var hash = List[Long]
-            //hash = hashfamily.hash(shingle)
-            val hash = shingle.hashCode()
+            val hash = hasher.hash(shingle)
+            //val hash = shingle.hashCode()
             //println(hash)
 
-            MINHASH = Math.min(MINHASH, hash)
+            //MINHASH = Math.min(MINHASH, hash)
 
             // Update minhash signature
-            /*for (j <- 0 to hash.length()) {
-              if (hash(i) < MINHASH(j)) {
-                MINHASH(j) = hash(j)
-                hashval(j) = shingle
-              }
-            }*/
+            for (j <- 0 to hash.length - 1) {
+              MINHASH(j) = Math.min(MINHASH(j), hash(j))
+            }
             shinglect += 1
           }
 
           if (shinglect < MAXLEN && shinglect > MINLEN) {
-            (MINHASH, (id, sentence))
+            val r = new Random(sigseed)
+            var list = ListBuffer[(String, (String, String))]()
+            for (j <- 0 to N - 1) {
+              var signature = ""
+              for (i <- 0 to K - 1) {
+                val x = r.nextInt(NHASH)
+                signature += MINHASH(x).toString()
+              }
+              println(signature)
+              list += ((signature, (id, sentence)))
+            }
+            //(MINHASH, (id, sentence))
+            list.toList
           } else {
-            (0, 0)
+            List()
           }
         } else {
-          (0, 0)
+          List()
         }
-        //val signatures = zdata.flatMap(v => model.hashFunctions.flatMap(h => List(((v._2, h._2 % numBands),h._1.minhash(v._1))))).cache()
       })
-      .filter(t => {
+      /*.filter(t => {
         t._1 != 0
-      })
+      })*/
       .groupByKey()
+      .map(t => {
+        var hashmap = new HashMap[String, (String, String)]
+        t._2.foreach(x => {
+          if (!hashmap.contains(x._2)) {
+            hashmap.put(x._2, x)
+          }
+        })
+        (t._1, hashmap.keys.toList)
+      })
       .filter(t => t._2.size > 1)
       //.collect()
       .saveAsTextFile(output)
       //.foreach(println)
-    //val rdd = sc.parallelize(data)
 
   }
 }
--- src/main/Scala/spark/hash/hasher.scala
+++ src/main/Scala/spark/hash/hasher.scala
@@ -3,8 +3,9 @@ package spark.hash
 /**
   * Created by jackzhang on 5/16/17.
   */
-/*import scala.util.Random
-import org.apache.spark.mllib.linalg.SparseVector
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+//import org.apache.spark.mllib.linalg.SparseVector
 
 /**
     * simple hashing function. defined by ints a, b, p, m
@@ -13,37 +14,62 @@ import org.apache.spark.mllib.linalg.SparseVector
     * m is the number of hash bins
   */
 
-class Hasher(a : Int, b : Int, p : Int, m : Int) extends Serializable {
+class MultipleShiftHash(m : Int, seeds : List[Long]) extends Serializable {
 
-  override def toString(): String = "(" + a + ", " + b + ")";
+  private val MAXLENGTH = 10000
+  private var acoeffmatrix = Array.ofDim[Long](seeds.length, MAXLENGTH)
+  private var bcoeffmatrix = Array.ofDim[Long](seeds.length, MAXLENGTH)
 
-  def hash(x: Int): Int = {
-    (((a.longValue * x) + b) % p).intValue % m
+  // Constructor
+  for (i <- 0 to seeds.length - 1) {
+    val r = new Random(seeds(i))
+    for (j <- 0 to MAXLENGTH - 1) {
+      acoeffmatrix(i)(j) = r.nextLong()
+      bcoeffmatrix(i)(j) = r.nextLong()
+    }
   }
 
-  def minhash(v: SparseVector): Int = {
+  //override def toString(): String = "(" + a + ", " + b + ")";
+
+  def hash(v: List[Long]): List[Long] = {
+    //(((a.longValue * x) + b) % p).intValue % m
+    var hashvec = ListBuffer[Long]()
+    var sum:Long = 0
+    for (s <- 0 to seeds.length - 1) {
+      for (i <- 0 to Math.min(v.length, MAXLENGTH) - 1) {
+        var a = acoeffmatrix(s)(i)
+        var b = bcoeffmatrix(s)(i)
+
+        // a has to be odd.
+        if (a % 2 == 0) {
+          a += 1
+        }
+        sum += v(i) * a + b
+      }
+      hashvec += (sum >> (64 - m))
+    }
+    hashvec.toList
+  }
+
+  def hash(s: String) : List[Long] = {
+    val b = s.getBytes()
+    // Long is 64 bits.
+    val longbytes = 64/8
+    var v = Array.ofDim[Long](b.length/longbytes + 1)
+    for (i <- 0 to b.length - 1) {
+      v(i/longbytes) |= ((b(i) & 0xff) << (longbytes*(i%longbytes)))
+    }
+    hash(v.toList)
+  }
+
+  /*def minhash(v: SparseVector): Int = {
     v.indices.map(i => hash(i)).min
-  }
+  }*/
 }
 
 object Hasher {
   /** create a new instance providing p and m. a and b random numbers mod p */
-  def create(p: Int, m: Int) = new Hasher(a(p), b(p), p, m)
+  def create(m : Int, seeds : List[Long]) = new MultipleShiftHash(m, seeds)
 
-
-  /** create a seed "a" */
-  def a(p: Int): Int = {
-    val r = new Random().nextInt(p)
-    if (r == 0)
-      a(p)
-    else
-      r
-  }
-
-  /** create a seed "b" */
-  def b(p: Int): Int = {
-    new Random().nextInt(p)
-  }
-
-}*/
+}
 
